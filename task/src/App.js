974b18482ab739a5e62af1ab6f0818751f257fc9
Anirudh <anirudhy@kgpian.iitkgp.ac.in>
2022-04-27 18:06:02 +0530

    Pointers used

--- task/src/App.js
+++ task/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Tablecom from './Components/Tablecom';
 import ListGroup from 'react-bootstrap/ListGroup'
 import { Button } from '@mui/material';
+import Radio from '@mui/material/Radio';
 
 
 function App() {
@@ -47,7 +48,9 @@ function App() {
       <ListGroup className='App__listGroup' as="ol" numbered>
         <ListGroup.Item as="li">Table showing data ftecched from given link</ListGroup.Item>
         <ListGroup.Item as="li">Cliclinh <Button variant="outlined" style={{ borderColor: "black", color: "black", width: "10px", margin: "5px 5px 0 0" }}>Sort</Button> will only give list of the chosen column</ListGroup.Item>
-        <ListGroup.Item as="li">Clicking <Button variant="outlined"  style={{ borderColor: "blue", color: "blue", width: "10px", margin: "5px 5px 0 0" }}>Show</Button> will sort the table according to the chosen column</ListGroup.Item>
+        <ListGroup.Item as="li">Clicking <Button variant="outlined" style={{ borderColor: "blue", color: "blue", width: "10px", margin: "5px 5px 0 0" }}>Show</Button> will sort the table according to the chosen column</ListGroup.Item>
+        <ListGroup.Item as="li">Click <Radio /> and enter the value by which you want to filter in the text field</ListGroup.Item>
+        <ListGroup.Item as="li">Click SHOW ALL<Radio /> and click Search button to get initial table </ListGroup.Item>
       </ListGroup>
       {data.length === 0 && list.length === 0 ? <h2>Please Wait...</h2> : <Tablecom columnNames={data} listData={list} />}
     </div>
